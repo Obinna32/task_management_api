8d96aeb48e52755314f6364691cffeafa3bcb3ca
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-02-11 13:05:46 +0100

    Chore: configure config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -116,3 +116,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework Configuration
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
+
