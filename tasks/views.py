fafce79afb3dd78e792cb5a9c6c3e8fa37c9306a
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-02-12 11:45:48 +0100

    feat: implement task CRUD endpoints

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing task instances.
+    This one class handles:
+    - GET /api/tasks/ (List)
+    - POST /api/tasks/ (Create)
+    - GET /api/tasks/<id>/ (Retrieve)
+    - PUT/PATCH /api/tasks/<id>/ (Update)
+    - DELETE /api/tasks/<id>/ (Delete)
+    """
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Return tasks only for the authenticated user
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        #create a nw task and automatically set the user
+        serializer.save(user=self.request.user)
