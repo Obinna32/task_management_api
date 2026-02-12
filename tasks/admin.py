86156c51c1fb64710a6c76417972212a6b26d67b
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-02-12 01:05:47 +0100

    feat: implement task model

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'is_completed', 'created_at')
+    list_filter = ('is_completed', 'created_at')
+    search_fields = ('title', 'description')
